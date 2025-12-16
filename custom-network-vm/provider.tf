4d2013a3ede39b13e1ba169ef728d04e8c950907
Shubham <sak2944@gmail.com>
2025-12-16 19:10:18 +0530

    Update provider.tf

--- custom-network-vm/provider.tf
+++ custom-network-vm/provider.tf
@@ -1,5 +1,5 @@
 provider "azurerm" {
     features {}
-    subscription_id = "28e1e42a-4438-4c30-9a5f-7d7b488fd883"
+    subscription_id = "f4733f9e-72aa-4d8f-b4bb-22ba87f56d23"
     resource_provider_registrations = "none"
 }
